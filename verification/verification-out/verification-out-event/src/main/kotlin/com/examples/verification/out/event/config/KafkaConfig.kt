97da691c4448e1c8508bea2f2bb5d74bdb98df34
Vladlens Filimonovs <vladlenfilimonov@gmail.com>
2023-09-10 15:03:30 +0300

    Events Config fix

--- verification/verification-out/verification-out-event/src/main/kotlin/com/examples/verification/out/event/config/KafkaConfig.kt
+++ verification/verification-out/verification-out-event/src/main/kotlin/com/examples/verification/out/event/config/KafkaConfig.kt
@@ -1,5 +1,6 @@
 package com.examples.verification.out.event.config
 
+import com.examples.verification.out.event.model.ConfirmVerificationEvent
 import com.examples.verification.out.event.model.CreateVerificationEvent
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
@@ -15,5 +16,11 @@ class KafkaConfig {
     fun createVerificationEventProducerTemplate(properties: KafkaProperties): ReactiveKafkaProducerTemplate<String, CreateVerificationEvent> {
         val props = properties.buildProducerProperties()
         return ReactiveKafkaProducerTemplate<String, CreateVerificationEvent>(SenderOptions.create(props))
+    }
+
+    @Bean
+    fun confirmVerificationEventProducerTemplate(properties: KafkaProperties): ReactiveKafkaProducerTemplate<String, ConfirmVerificationEvent> {
+        val props = properties.buildProducerProperties()
+        return ReactiveKafkaProducerTemplate<String, ConfirmVerificationEvent>(SenderOptions.create(props))
     }
 }
